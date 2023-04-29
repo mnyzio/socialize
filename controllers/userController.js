52992fc08570215a2f5bc5af011d1419ce7425f1
mnyzio <nyziu84@gmail.com>
2023-04-29 16:57:34 -0400

    get single user by ID

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,9 +9,22 @@ module.exports = {
             .then((users) => res.json(users))
             .catch((err) => res.status(500).json(err));
     },
+    // Get single user by passing ID into query params
+    getSingleUser(req, res) {
+        console.log(`In singe user route with user ID ${req.params.userId}`)
+        User.findOne({ _id: req.params.userId })
+            .select('-__v')            
+            .then((user) =>                 
+                !user
+                    ? res.status(404).json({ message: 'No user with that ID' })                
+                    : res.status(200).json(user)
+            )
+            .catch (err => res.status(500).json(err));
+    },
+    // Create user
     createUser(req,res) {
         User.create(req.body)
         .then((user) => res.json(user))
         .catch((err) => res.status(500).json(err));
-    },
+    },    
 };
