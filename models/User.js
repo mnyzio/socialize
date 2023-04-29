5a344b5f2e8a6d10725caf4c96ab0a2a598918d4
mnyzio <nyziu84@gmail.com>
2023-04-28 23:34:37 -0400

    started schema and model

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema (
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            // Must match a valid email address (look into Mongoose's matching validation)
+        },
+        thoughts: {
+            // Array of `_id` values referencing the `Thought` model
+        },
+        friends: {
+            // Array of `_id` values referencing the `User` model (self-reference)
+        }
+        // Create a virtual called `friendCount` that retrieves the length of the user's `friends` array field on query.
+    }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
---
+++ models/index.js
@@ -0,0 +1,5 @@
+const User = require('./User');
+// Add thoughts
+
+
+module.exports = { User };
